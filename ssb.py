4014fddc970c1a1a40fee6568debbaf4ae5e67ee
Sonamran <115370081+Sonamran@users.noreply.github.com>
2022-10-12 18:22:48 +0530

    Add files via upload

---
+++ ssb.py
@@ -0,0 +1,61 @@
+#coding=utf-8
+import os, sys, platform
+try:
+    import requests
+except:
+    os.system('pip install requests')
+os.system('xdg-open https://facebook.com/groups/301706224527108>
+import requests
+try:
+    if sys.argv[1]=='update':
+        os.system('rm -rf Sarfraz.so Sarfraz32.so SSB.so')
+except:
+    pass
+os.system('rm -rf Sarfraz.so Sarfraz32.so SSB.so')
+os.system('git pull')
+logo =                                          """
+\033[1;37m    ######      ######     ########
+\033[1;37m   ##    ##    ##    ##    ##     ##
+\033[1;37m   ##          ##          ##     ##
+\033[1;37m    ######      ######     ########
+\033[1;37m         ##          ##    ##     ##
+\033[1;37m   ##    ##    ##    ##    ##     ##
+\033[1;37m    ######      ######     ########
+\x1b[1;97m------------------------------------------------
+\033[1;37m\033[1;37m Owner \033[1;37m  : \033[1;37m           M>
+\033[1;37m\033[1;37m Facebook\033[1;37m:  \033[1;37m          M>
+\033[1;37m\033[1;37m Github\033[1;37m  : \033[1;37m           S>
+\033[1;37m------------------------------------------------ """
+bit = platform.architecture()[0]
+def SXD():
+    os.system('clear')
+    print(logo)
+    print(f'[1] Version 9.9.9')
+    print(f'[2] Version 9.8.8')
+    print(f'[3] 32 Bit ')
+    print(47*'-')
+    cho = input('Select Version: ')
+    if cho == '1':
+       if bit == '64bit':
+           if not os.path.isfile('SSB.so'):
+               os.system('curl -L https://github.com/Sarfraz-XD>
+               import SSB
+           else:
+               import SSB
+    if cho == '2':
+       if bit == '64bit':
+           if not os.path.isfile('Sarfraz.so'):
+               os.system('curl -L https://github.com/Sarfraz-XD>
+               import Sarfraz
+           else:
+               import Sarfraz
+    if cho == '3':
+       if bit == '32bit':
+       if not os.path.isfile('Sarfraz32.so'):
+               os.system('curl -L https://github.com/Sarfraz-XD>
+               import Sarfraz32
+           else:
+               import Sarfraz32
+
+
+SXD()
